454ee48185a12c03cb41de765771b3b85ea6a1b1
ianasten <38948409+ianasten@users.noreply.github.com>
2019-06-11 17:23:25 -0300

    Update Celular

--- Celular.h
+++ Celular.h
@@ -1,18 +1,22 @@
 #ifndef CELULAR_H_INCLUDED
 #define CELULAR_H_INCLUDED
-
+#include "Plano.h"
 using namespace std;
 
 class Celular{
 private:
     double _numero;
-public: // nao sei
     Cliente _cliente;              // nao sei se e public or private
     Plano _plano                   // nao sei
     vector<Ligacao> _ligacoes;     // nao sei
     static double _proxNumCelular; // nao sei
 
+public: // nao sei
+
+
 
 };
+
+double Celular::_proxNumCelular = alguma coisa;
 
 #endif // CELULAR_H_INCLUDED
